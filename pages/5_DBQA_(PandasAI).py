a9acf3676aadd46c85e47408eb59406a7fffe6bb
Da-Wei Lee <daviddwlee84@gmail.com>
2023-12-28 16:02:18 +0800

    Add multi-turn chat for PandasAI

--- pages/5_DBQA_(PandasAI).py
+++ pages/5_DBQA_(PandasAI).py
@@ -4,7 +4,7 @@ import pandas as pd
 from dotenv import load_dotenv
 import os
 import pandas as pd
-from pandasai import SmartDataframe
+from pandasai import SmartDataframe, Agent, SmartDatalake
 from pandasai.llm import OpenAI, AzureOpenAI
 
 llm = OpenAI(api_token="YOUR_API_TOKEN")
@@ -17,31 +17,6 @@ load_dotenv(os.path.join(curr_dir, "../.env"))
 st.set_page_config(page_title="Database Question Answering (using PandasAI)")
 
 st.title("Database Question Answering (using PandasAI)")
-
-# sqlite3.ProgrammingError: SQLite objects created in a thread can only be used in that same thread. The object was created in thread id 47584 and this is thread id 15496.
-if "dbqa_pandasai_uploaded_file" not in st.session_state:
-    st.session_state.dbqa_pandasai_uploaded_file = None
-    st.session_state.dbqa_pandasai_data = None
-
-uploaded_file = st.file_uploader(
-    "Data you want to query (support CSV, Parquet, and Json).",
-    accept_multiple_files=False,
-)
-
-if uploaded_file is None:
-    st.session_state.dbqa_pandasai_messages = []
-else:
-    if st.session_state.dbqa_pandasai_uploaded_file != uploaded_file:
-        st.session_state.dbqa_pandasai_messages = []
-        st.session_state.dbqa_pandasai_uploaded_file = uploaded_file
-
-        if uploaded_file.name.endswith(".csv"):
-            st.session_state.dbqa_pandasai_data = pd.read_csv(uploaded_file)
-        elif uploaded_file.name.endswith(".parquet"):
-            st.session_state.dbqa_pandasai_data = pd.read_parquet(uploaded_file)
-        else:
-            st.error("Please provide valid file type.")
-            st.stop()
 
 with st.sidebar:
     openai_selection: Literal["OpenAI", "Azure OpenAI"] = st.selectbox(
@@ -92,6 +67,89 @@ with st.sidebar:
             type="default",
         )
 
+if openai_selection == "OpenAI":
+    if not st.session_state.openai_api_key:
+        st.warning("🥸 Please add your OpenAI API key to continue.")
+        st.stop()
+
+    llm = OpenAI(
+        model="gpt-35-turbo",
+        api_token=st.session_state.openai_api_key,
+    )
+
+elif openai_selection == "Azure OpenAI":
+    if not st.session_state.azure_openai_api_key:
+        st.warning("🥸 Please add your Azure OpenAI API key to continue.")
+        st.stop()
+
+    llm = AzureOpenAI(
+        model="gpt-35-turbo",
+        api_token=st.session_state.azure_openai_api_key,
+        azure_endpoint=st.session_state.azure_openai_endpoint,
+        api_version=st.session_state.azure_openai_version,
+        deployment_name=st.session_state.azure_openai_deployment_name,
+        is_chat_model=True,
+    )
+
+if "dbqa_pandasai_uploaded_file" not in st.session_state:
+    st.session_state.dbqa_pandasai_uploaded_file = None
+    st.session_state.dbqa_pandasai_data = None
+
+chat_mode: Literal["Single Turn", "Multi Turn"] = st.selectbox(
+    "Chat Mode", ["Single Turn", "Multi Turn"]
+)
+
+uploaded_file = st.file_uploader(
+    "Data you want to query (support CSV, Parquet, and Json).",
+    accept_multiple_files=False,
+)
+
+if uploaded_file is None:
+    st.session_state.dbqa_pandasai_messages = []
+    st.session_state.pandasai_df = None
+else:
+    config = {
+        "llm": llm,
+        "verbose": True,
+        "save_charts": True,
+        "save_charts_path": os.path.join(curr_dir, "../exports/charts/"),
+        "open_charts": False,
+        "enable_cache": True,
+    }
+
+    if st.session_state.dbqa_pandasai_uploaded_file != uploaded_file:
+        st.session_state.dbqa_pandasai_messages = []
+        st.session_state.dbqa_pandasai_uploaded_file = uploaded_file
+
+        if uploaded_file.name.endswith(".csv"):
+            st.session_state.dbqa_pandasai_data = pd.read_csv(uploaded_file)
+        elif uploaded_file.name.endswith(".parquet"):
+            st.session_state.dbqa_pandasai_data = pd.read_parquet(uploaded_file)
+        else:
+            st.error("Please provide valid file type.")
+            st.stop()
+
+        st.session_state.chat_mode = chat_mode
+        if chat_mode == "Single Turn":
+            st.session_state.pandasai_df = SmartDatalake(
+                [st.session_state.dbqa_pandasai_data], config=config
+            )
+        elif chat_mode == "Multi Turn":
+            st.session_state.pandasai_df = Agent(
+                [st.session_state.dbqa_pandasai_data], config=config
+            )
+
+    if st.session_state.chat_mode != chat_mode:
+        st.session_state.dbqa_pandasai_messages = []
+        if chat_mode == "Single Turn":
+            st.session_state.pandasai_df = SmartDatalake(
+                [st.session_state.dbqa_pandasai_data], config=config
+            )
+        elif chat_mode == "Multi Turn":
+            st.session_state.pandasai_df = Agent(
+                [st.session_state.dbqa_pandasai_data], config=config
+            )
+
 
 if "dbqa_pandasai_messages" not in st.session_state:
     st.session_state["dbqa_pandasai_messages"] = []
@@ -119,38 +177,14 @@ for msg in st.session_state.dbqa_pandasai_messages:
 
 # https://streamlit.io/generative-ai
 # TODO: make response streaming https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps#build-a-simple-chatbot-gui-with-streaming
-if prompt := st.chat_input():
-    if openai_selection == "OpenAI":
-        if not st.session_state.openai_api_key:
-            st.warning("🥸 Please add your OpenAI API key to continue.")
-            st.stop()
-
-        llm = OpenAI(
-            model="gpt-35-turbo",
-            api_token=st.session_state.openai_api_key,
-        )
-
-    elif openai_selection == "Azure OpenAI":
-        if not st.session_state.azure_openai_api_key:
-            st.warning("🥸 Please add your Azure OpenAI API key to continue.")
-            st.stop()
-
-        llm = AzureOpenAI(
-            model="gpt-35-turbo",
-            api_token=st.session_state.azure_openai_api_key,
-            azure_endpoint=st.session_state.azure_openai_endpoint,
-            api_version=st.session_state.azure_openai_version,
-            deployment_name=st.session_state.azure_openai_deployment_name,
-            is_chat_model=True,
-        )
-
+if prompt := st.chat_input(disabled=st.session_state.pandasai_df is None):
     st.session_state.dbqa_pandasai_messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    df = SmartDataframe(st.session_state.dbqa_pandasai_data, config={"llm": llm})
     # TODO: can't properly save image
-    response = df.chat(prompt)
-    st.write(response)
+    response = st.session_state.pandasai_df.chat(prompt)
+    # Debug
+    # st.write(response)
     st.session_state.dbqa_pandasai_messages.append(
         {"role": "assistant", "content": response}
     )
